c0fdc8d966e4ca904739aa20f74795653a6cce10
cgusb <cgusbecker@gmail.com>
2022-01-13 00:29:33 -0500

    add `resize_by_nrows()` and `tesselate()`

--- src/helpers.py
+++ src/helpers.py
@@ -25,7 +25,15 @@ def plot_hist(img, chan=None, xlims=(0, 1), cumul=False, vlines=None):
             ax.axvline(v, color='r')
     return fig, ax
 
-def resize_by_row(img_to_resize, img_to_match):
+def resize_by_nrows(img, nrows):
+    img_resized = transform.resize(
+        img, 
+        (nrows, img.shape[1] * nrows / img.shape[0]), 
+        anti_aliasing=True
+    )
+    return img_resized
+
+def resize_by_img(img_to_resize, img_to_match):
     rows_match = img_to_match.shape[0]
     rows_resize = img_to_resize.shape[0]
     img_resized = transform.resize(
@@ -34,3 +42,19 @@ def resize_by_row(img_to_resize, img_to_match):
         anti_aliasing=True
     )
     return img_resized
+
+def tesselate(img, canvas_size=(1080, 1920, 3)):
+    canvas = np.zeros(canvas_size)
+    nrows, ncols, nchans = img.shape
+    img_resized = resize_by_nrows(img, canvas_size[0])
+    print(img_resized.shape)
+    ntiles = canvas_size[1] // nrows + 1
+    for i in range(1, ntiles):
+        canvas[:, (i - 1) * ncols:i * ncols, :] = img_resized.copy() 
+    canvas[:, (i - 1) * ncols:, :] = img_resized[:, :canvas_size[1] % ncols].copy() 
+    return canvas
+
+def pinkify_img(img: np.ndarray, pink_s=0.08):
+    pinkified_img = img.copy()
+    return pinkified_img
+
